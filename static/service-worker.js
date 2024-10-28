e8f795e657c32e4dfbd9e238e529605757fe6ebb
Kevin Simorangkir <104813623+kevinsimorangkir21@users.noreply.github.com>
2024-10-28 23:51:14 +0700

    Update service-worker.js

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,60 +1,31 @@
 const CACHE_NAME = 'gradebook-cache-v1';
-const urlsToCache = [
-    '/',
-    '/static/css/styles.css',
-    '/static/js/script.js',
-    '/static/images/logo.png',
-    // Add more static assets that should be cached here
-];
 
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then((cache) => {
-                console.log('Opened cache');
-                return cache.addAll(urlsToCache);
-            })
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        '/',
+        '/static/style.css',
+        '/static/script.js'
+      ]);
+    })
+  );
 });
 
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request)
-            .then((response) => {
-                if (response) {
-                    return response;  // Return cached version
-                }
-
-                // If the request is not in cache, fetch it from the network
-                return fetch(event.request).then((networkResponse) => {
-                    // If the response is a valid one, clone it and store it in cache
-                    if (!networkResponse || networkResponse.status !== 200 || networkResponse.type !== 'basic') {
-                        return networkResponse;
-                    }
-
-                    const responseToCache = networkResponse.clone();
-                    caches.open(CACHE_NAME)
-                        .then((cache) => {
-                            cache.put(event.request, responseToCache);
-                        });
-
-                    return networkResponse;
-                });
-            })
-    );
-});
-
-self.addEventListener('activate', (event) => {
-    const cacheWhitelist = [CACHE_NAME];
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.map((cacheName) => {
-                    if (cacheWhitelist.indexOf(cacheName) === -1) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
+  event.respondWith(
+    fetch(event.request).then((response) => {
+      // If we got a valid response, clone it and update the cache
+      if (response && response.status === 200 && response.type === 'basic') {
+        const responseToCache = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseToCache);
+        });
+      }
+      return response;
+    }).catch(() => {
+      // If the network request fails, try to serve from cache
+      return caches.match(event.request);
+    })
+  );
 });
